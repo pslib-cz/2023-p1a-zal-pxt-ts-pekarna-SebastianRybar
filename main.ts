a4163b8bb62e3cef39e3e9183a2115bdc7611089
SebastianRybar <147390978+SebastianRybar@users.noreply.github.com>
2024-03-04 17:39:29 +0100

    HOTOVO

--- main.ts
+++ main.ts
@@ -9,21 +9,34 @@
     odebrané množství "energie":
     3500 * (600 / 3600) === 3500 * (1/6) === 583,333W
 */
-type Odber = {
-    cas: number, //čas je v sekundách
-    spotreba: number //spotřeba ve watech za hodinu
-}
-const cena: number = 6.70; // Kč / kWh
-let data: Array<Odber> = [
-    { cas: 600, spotreba: 3500 },
-    { cas: 120, spotreba: 0 },
-    { cas: 300, spotreba: 1700 }, // data[2].cas
-    { cas: 60, spotreba: 0 },
-    { cas: 800, spotreba: 1500 },
-]
+const cena: number = 6.75
 
-for (const peceme of data){
-    console.logValue("čas: ", peceme.cas);
-    console.logValue("spotřeba: ", peceme.spotreba);
-    console.logValue("spotřebováno: ", peceme.spotreba / 3600 * peceme.cas);
+type Odber = {
+    time: number; //cas sekundy
+    prikon: number //watt 6,75
+}
+
+const data: Array<Odber> = [
+    { time: 600, prikon: 3500 },
+    { time: 120, prikon: 0 },
+    { time: 300, prikon: 1700 },
+    { time: 600, prikon: 0 },
+    { time: 800, prikon: 1500 },
+
+]
+let celek: number = 0
+for (let i: number = 0; i < data.length; i++) {
+    celek += data[i].time
+    if (i === 4) {
+        celek /= 3600
+    }
+}
+let celek2: number = 0
+for (let j: number = 0; j < data.length; j++) {
+    celek2 += data[j].prikon
+    if (j === 4) {
+        celek2 /= 1000
+        console.log(celek2 * celek / 5 + "" + " prumerna cena v Kč")
+        console.log(celek * celek2 * cena + "" + " cena v Kč")
+    }
 }
